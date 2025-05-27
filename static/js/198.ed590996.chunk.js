70879778b56acf5f74651b4ab61cd845c091cdad
Adhouib <azizdhouibcr7@gmail.com>
2025-05-27 23:49:32 +0200

    Updates

---
+++ static/js/198.ed590996.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktranspilot_admin=self.webpackChunktranspilot_admin||[]).push([[198],{1198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.ed590996.chunk.js.map
